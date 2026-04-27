499cbe91f745b8c50f532d90c9c4f62f92bd9578
Simjrn <simonjenni374@gmail.com>
2026-04-27 17:21:43 +0000

    Add writewords and pickwords

--- questions.py
+++ questions.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import re
+import streamlit_sortables as s_t
 
-line = "{type:PickMissingWord|question:Allegra, co ___vai___?|options:[Bain][Allegra][Co vai?][vai]|answer:vai|id:AoYKyORCZiOU}"
+line = "{type:PickWords|question:El è bain|options:[is][He][She][well]|answer:Heiswell|answerviewable:He is well|id:Xr6TDICdglEB}"
 @st.fragment
 def render_question(line):
     line = line.split('|')
@@ -16,17 +17,17 @@ def render_question(line):
         result = st.container(border=False)
         if one:
             if options[0] == answer:
-                result.info("Correct!")
+                result.success("Correct!")
             else:
                 result.error("No")
         elif two:
             if options[1] == answer:
-                result.info("Correct!")
+                result.success("Correct!")
             else:
                 result.error("No")
         elif three:
             if options[2] == answer:
-                result.info("Correct!")
+                result.success("Correct!")
             else:
                 result.error("No")
     elif 'PickMissingWord' in line[0]:
@@ -40,24 +41,50 @@ def render_question(line):
         result = st.container(border=False)
         if one:
             if options[0] == answer:
-                result.info("Correct!")
+                result.success("Correct!")
             else:
                 result.error("No")
         elif two:
             if options[1] == answer:
-                result.info("Correct!")
+                result.success("Correct!")
             else:
                 result.error("No")
         elif three:
             if options[2] == answer:
-                result.info("Correct!")
+                result.success("Correct!")
             else:
                 result.error("No")
         elif four:
             if options[3] == answer:
-                result.info("Correct!")
+                result.success("Correct!")
             else:
                 result.error("No")
+    elif 'WriteWords' in line[0]:
+        st.header("Translate: '"+line[1][9:]+ "'")
+        answer = st.text_area("")
+        submit = st.button("Submit")
+        if submit:
+            if answer == line[2][7:]:
+                st.success("Correct")
+            else:
+                st.error("")
+    elif "PickWords" in line[0]:
+        words = line[2][9:-1].split("][")
+        st.header("Translate: '"+line[1][9:]+ "'")
+        items = [
+            {"header": "Your answer", "items": []},
+            {"header": "Word bank", "items": words}
+        ]
+        sorted_items = s_t.sort_items(items, multi_containers=True)
+        col1, col2, col3 = st.columns([1,2,1])
+        if col2.button("Submit", width="stretch", type="primary"):
+            if "".join(sorted_items[0]["items"]) == line[3][7:]:
+                st.success("Correct!")
+            else:
+                st.error("")
+        
+
+
 
 
 
